2d889e3731c4f3888b8b430da59f46be8a0dadaf
Arav Salter Patel <arav.patel@oaknorth.co.uk>
2023-01-13 07:43:33 +0000

    Added in for the first 4 rows

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,13 +3,22 @@ import { render, screen } from "@testing-library/react";
 import App from "./App.jsx";
 
 describe("Loan applications dashboard view", () => {
-  test("displays a hello world title", () => {
-    render(<App />);
-    screen.getByText("hello world");
-  });
-
   test("displays the first loan application data", () => {
     render(<App />);
     screen.getByText("Alice Jones");
+    screen.getByText('17 Fore St, Somerset, TA21 8AA')
+    screen.getByText('7.2%')
+    screen.getByText('£500,000')
+    screen.getByText('2022-11-15')
+  });
+
+  test("displays the second loan application data", () => {
+    render(<App />);
+    screen.getByText("Daffy Duck");
+    screen.getByText('65 Rowlands Rd, West Sussex, BN11 3JN')
+    screen.getByText('COMMERCIAL')
+    screen.getByText('5.4%')
+    screen.getByText('£870,000')
+    screen.getByText('2022-11-16')
   });
 });
